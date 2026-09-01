0b1b25f3dc656f4bf74168004a4196f963743ec8
JoaoVictor-Noschang <joaovictornoschangsilva601@gmail.com>
2026-09-01 10:36:20 -0400

    Commit inicial do repositório

---
+++ aula01_1708/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+
+    char letra = 'A';
+
+    int idade;
+    float salario;
+    double valor;
+    //char letra;
+
+    idade = 10;
+    salario = 2500.20;
+    valor = 599.99;
+    /* letra = 'j'; */
+
+
+
+    printf("a letra guardada e %c", letra);
+
+
+    return 0;
+}
---
+++ aula02_18_08/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+    /*
+    char letra;
+
+    printf("Digite uma letra: ");
+    scanf("%c", &letra);
+
+    printf("%c", letra);
+    */
+
+    int anoNasc;
+    printf("Digite o seu ano de nascimento:");
+
+    scanf("%d", &anoNasc);
+
+    printf("Voce nasceu em: %d", anoNasc);
+
+}
